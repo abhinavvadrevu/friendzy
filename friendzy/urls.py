2e7c0c836d5ca24ede1e46e5a182a3bf77b744a0
User <abhinav@ubuntu.(none)>
2013-03-31 23:16:03 -0700

    removing django gcm dependency

--- friendzy/urls.py
+++ friendzy/urls.py
@@ -16,6 +16,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('gcm.urls')),
-    #url(r'gcmtest', 'users.views.gcmtest'),
+    #url(r'', include('gcm.urls')),
+    url(r'gcmtest', 'users.views.gcmtest'),
 )
