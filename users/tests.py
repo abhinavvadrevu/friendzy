70d8bbc7ca23226397d579554f29161b3136f3f7
hong <hongnhung1636@gmail.com>
2013-04-19 02:34:05 -0700

    update 83% unittest code coverage for models.py

--- users/tests.py
+++ users/tests.py
@@ -7,6 +7,7 @@
 import sys, os
 sys.path = [os.path.abspath(os.path.dirname(__file__))] + sys.path
 import unittest
+import datetime as dt
 from users import models
 
 
@@ -21,8 +22,77 @@ class UserTest(unittest.TestCase):
     def test_user_exists1(self):
         # objects.create creates and saves a db entry in one step
         models.User.objects.create_user('user1', ['some', 'list', 'items'],'123', '1234567')
-        self.assertTrue(models.User.objects.user_exists('user1'),"user doesn't not exit")
-  
+        self.assertTrue(models.User.objects.user_exists('user1'),"user exits")
+        
+    def test_user_exists2(self):
+        # objects.create creates and saves a db entry in one step
+        models.User.objects.create_user('user1', ['some', 'list', 'items'],'123', '1234567')
+        self.assertFalse(models.User.objects.user_exists('user2'),"user does not exit")
+        
+    def test_user_exits3(self):
+        user = models.User.objects.create_user('an_id', ['some', 'list', 'items'], '1234', '12345678')
+        self.assertEqual(user.facebook_id, 'an_id')
+        self.assertEqual(user.phone_number, '12345678')
+        self.assertEqual(user.friends, ['some', 'list', 'items'])
+        self.assertEqual(user.regId, '1234')
+        
+    def test_user_exits4(self):
+        user = models.User.objects.create_user('an_id', ['some', 'list', 'items'], '1234', '12345678')
+        self.assertNotEqual(user.facebook_id, 'another_id')
+        self.assertEqual(user.phone_number, '12345678')
+        self.assertNotEqual(user.friends, ['some', 'other', 'items'])
+        self.assertEqual(user.regId, '1234')
+        
+    def test_user_exits5(self):
+        models.User.objects.create_user('user_test1', ['one', 'two', 'tree'], 'a1234', '123469294')
+        sample_model = models.User.objects.get_user('user_test1')
+        self.assertEqual(sample_model.friends, ['one', 'two', 'tree'])
+        self.assertEqual(sample_model.facebook_id,'user_test1')
+        self.assertEqual(sample_model.phone_number, '123469294')
+        self.assertEqual(sample_model.regId,'a1234')
+        
+        
+    def test_user_exits6(self):
+        trying1 = models.User.objects.create_user('user_test2', ['some', 'aaa', 'items'],'193', '357943')
+        self.assertEqual(trying1.login( ['some', 'aaa', 'items'], '193'), {'data': {}})
+        
+    def test_user_exit7(self):
+        trying1 = models.User.objects.create_user('an_id', ['some'],'123', '1234567')
+        trying2 = models.User.objects.create_user('some', ['an_id'],'195', '357943')
+        self.assertEqual(trying1.friends[0], trying2.facebook_id)
+        self.assertEqual(trying2.friends[0], trying1.facebook_id)
+        
+    def test_user_exists8(self):
+        a = models.UserManager()
+        self.assertFalse(a.user_exists('1'), 'user has not created yet')
+        
+    def test_user_exists9(self):
+        models.User.objects.create_user('yyy', ['some'],'123', '1234567')
+        app = models.Appeal()
+        self.assertEqual(app.get_regId('yyy'), '123')
+        app.uid = 'new'
+        app.friendid = 'old'
+        app.latitude = 37.879719
+        app.longitude = -122.260744
+        a = app.get_data('old', 37.879719,-122.260744)
+        self.assertEqual(a['meetingName'],'Chez Panisse')
+        self.assertEqual(a['meetingLocation'],{'latitude': 37.8795938, 'longitude': -122.2689348})
+        
+    def test_user_exists10(self):
+        models.User.objects.create_user('hongle1', ['anh1'],'30091', '77777')
+        models.User.objects.create_user('anh1', ['hongle1'],'26091', '66666')
+        self.assertEqual(models.Appeal.objects.notify('hongle1','anh1'), None)
+        self.assertFalse(models.Appeal.objects.appeal_exists('hongle1','anh1'), 'exists') 
+        data44 = {
+            "messageType": "initial",
+            "ownId": 'hongle1',
+            "data": {
+                "friendId": 'anh1',
+                "friendStatus": 'hello 1',
+                "ownStatus": 'hello 2'
+            }}
+        
+        
     def test_Status(self):
         status1 = models.Status()
         ans = "ABC"
@@ -31,7 +101,6 @@ class UserTest(unittest.TestCase):
     
     def test_Status2(self):
         status1 = models.Status()
-        status2 = models.Status()
         ans = "ABC"
         wrong = '123'
         status1.set_status(ans,True)
@@ -42,6 +111,17 @@ class UserTest(unittest.TestCase):
         ans = "ABC"
         status1.set_status(ans,True)
         self.assertEqual(status1.get_status(), ans)
+        
+    def test_Status4(self):
+        trying1 = models.User.objects.create_user('an_id', ['some'],'123', '1234567')
+        self.assertEqual(trying1.get_friend_statuses(), {})
+    
+    def test_statu5(self):
+        a = models.Status.objects.create_status()
+        self.assertEqual(a.status, '')
+        a.status = 'status'
+        self.assertEqual(a.status, 'status')
+        
         
     def test_chat1(self):
         models.Chat.objects.create_chat('userID', 'friendID')
@@ -54,6 +134,20 @@ class UserTest(unittest.TestCase):
     def test_chat3(self):
         models.Chat.objects.create_chat('userID', 'friendID')
         self.assertFalse(models.Chat.objects.chat_exists('userID', 'friendID1'), 'chat fail: wrong friend ID')
+        
+    def test_chat4(self):
+        a =  models.Chat.objects.create_chat('userID', 'friendID')
+        self.assertEqual(a.user1id, 'userID')
+        self.assertEqual(a.user2id, 'friendID')
+        a.add_message('userID', 'how are you?')
+        self.assertEqual(a.messages[0][0], 'how are you?')
+        
+    def test_chat5(self):
+        b = models.Chat.objects.create_chat('hong', 'anh')
+        b.add_message('anh', 'chat')
+        bb = models.Chat.objects.get_chat('hong','anh')
+        self.assertEqual(bb.messages[0][0], 'chat')
+        self.assertEqual(bb.messages[0][2], 'anh')
     
     def test_matching_status(self):
         status1 = 'abc'
@@ -76,6 +170,7 @@ class UserTest(unittest.TestCase):
         user5.status = models.Status()
         user5.status.set_status('123', True)
         self.assertNotEqual(user5.status.get_status(), '456')
+        
 
     def test_fbID(self):
         user = models.User()
@@ -126,6 +221,7 @@ class UserTest(unittest.TestCase):
         try1.status.set_status('math 1', True)
         self.assertNotEqual(try1.notification_message('try1', 'math 1', 'math'), 
                          'user try1 posted a message about \'math\':  "math 2"')
+
 
         
 class TestLocation(unittest.TestCase):
@@ -215,6 +311,47 @@ class TestLocation(unittest.TestCase):
         mid2 = pt2.midpoint(pt1)
         self.assertNotEqual(pt1.longitude, mid1.longitude)
         self.assertNotEqual(pt2.longitude, mid2.longitude)
+        
+    def test_set_location(self):
+        pt1 = models.Location()
+        pt1.set_location(12,13)
+        self.assertEqual(pt1.latitude, 12)
+        self.assertEqual(pt1.longitude, 13)
+        
+    def test_get_midpoint_method(self):
+        pt1 = models.Location()
+        pt1.set_location(12,13)
+        pt2 = models.Location()
+        pt2.set_location(14,15)
+        midpoint11 = pt1.midpoint(pt2)
+        self.assertEqual(midpoint11.latitude, 13.001912951949645)
+        self.assertEqual(midpoint11.longitude, 13.995969771506447)
+        
+    def test_get_meeting_location(self):
+        pt1 = models.Location()
+        pt1.set_location(37.879719,-122.260744)
+        pt2 = models.Location()
+        pt2.set_location(42.752,-122.489)
+        a = pt1.get_meeting_point(pt2)
+        self.assertEqual(a['latitude'], 40.3849469)
+        self.assertEqual(a['longitude'], -122.2815379)
+        self.assertEqual(a['name'], 'Main Street Diner')
+        self.assertEqual(a['address'], '3342 Main St Cottonwood, CA 96022')
+        
+        
+    def test_get_distance_method(self):
+        pt1 = models.Location()
+        pt1.set_location(37.879719,-122.260744)
+        pt2 = models.Location()
+        pt2.set_location(42.752,-122.489)
+        self.assertEqual(pt1.get_distance(pt2), 541.7774423815335)
+        
+    def test_gmc(self):
+        data77 = {'userId': 'qqq', 'friendId': '09483', 'userLocation': {'latitude':37.879719, 'longitude': -122.260744},
+                 'friendLocation':{ 'latitude': 42.752, 'longitude':-122.489}, 'meetingName': 'Soda Hall',
+                 'meetingLocation': {'latitude': 40.3849469, 'longitude': -122.2815379}}
+        models.User.objects.create_user('team', ['some', 'list', 'items'],'133','1111111')
+   
     
     def test_get_msm(self):
         user1 = models.Chat()
@@ -242,6 +379,40 @@ class TestLocation(unittest.TestCase):
         user1.add_message('user1', 'message from user1')
         self.assertNotEqual(user1.get_updates('user2'), user2.get_updates('user1'))
         
+    def test_add_msm_and_update(self):
+        chating1 = models.Chat()
+        chating1.user1id = 'user22'
+        chating1.user2id = 'user33'  
+        chating1.add_message('user22', 'hi')
+        self.assertEqual(chating1.messages[0][0], 'hi')
+        self.assertEqual(chating1.messages[0][2], 'user22')
+        a = chating1.get_updates('user33')
+        self.assertEqual(a[0][0], 'hi')
+        chating1.visited('user22')
+        today = dt.date.today()
+        self.assertEqual(chating1.user1lastvisit.year, today.year)
+        self.assertEqual(chating1.user1lastvisit.month,today.month)
+        self.assertEqual(chating1.user1lastvisit.day,today.day)
+        self.assertTrue(chating1.connected('user22'), 'user not connected')
+        self.assertFalse(chating1.connected('user33'), 'user connected')
+        
+    def test_sms(self):
+        models.User.objects.create_user('group00', ['some', 'aaaat', 'items'],'113','13333111')
+        models.User.objects.create_user('team00', ['some', 'list', 'items'],'133','1111111')
+        models.User.objects.subscriber('team00', 'add', 'test', ['group00'])
+        a = models.User.objects.get_user('group00') 
+        a.send_sms('hello')
+        a.setsms(13333111)
+        self.assertTrue(a.sms, 'sms set')
+        notify = a.notifyfollowers('test')
+        self.assertIsNone(notify, 'sent')
+        self.assertEqual(a.followers.keyval_set.all()[0].value, 'test')
+        self.assertEqual(a.followers.keyval_set.all()[0].key, 'team00')
+        b = a.appendkv('team00', 'math')
+        self.assertIsNone(b, 'subscribe')
+        self.assertEqual(a.followers.keyval_set.all()[1].value, 'math')
+        self.assertEqual(a.followers.keyval_set.all()[1].key, 'team00')
+        
     def test_meeting(self):
         data = {'userId': 'aaa', 'friendId': '123', 'userLocation': {'latitude':12, 'longitude': 14},
                  'friendLocation':{ 'latitude': 14, 'longitude':15}, 'meetingName': 'Soda Hall',
@@ -262,9 +433,19 @@ class TestLocation(unittest.TestCase):
         self.assertEqual(meeting.longitude,14)
         self.assertEqual(meeting.meeting_name,'Soda Hall')
         
+    def test_meeting2(self):
+        data = {'userId': 'bbb', 'friendId': '123', 'userLocation': {'latitude':12, 'longitude': 14},
+                 'friendLocation':{ 'latitude': 14, 'longitude':15}, 'meetingName': 'Soda Hall',
+                 'meetingLocation': {'latitude': 12, 'longitude': 14}}
+        a1 = models.Meeting.objects.create_meeting(data)
+        self.assertEqual(models.Meeting.objects.get_meetings(12,13), {'data': []})
         
-        
-        
+    def test_meeting3(self):
+        testm = models.Meeting()
+        self.assertEqual(testm.get_data()['longitude'], 0)
+        self.assertEqual(testm.get_data()['location_name'], '')
+        self.assertEqual(testm.get_data()['latitude'], 0)
+        self.assertEqual(testm.get_data()['attendees'], [])
         
         
         
