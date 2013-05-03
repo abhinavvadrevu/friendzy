9a473f068344f695d92904e2e05b940ae9e81410
Timmy Zhu <nattofriends@gmail.com>
2013-05-03 13:35:05 -0700

    back to sqlite3

--- friendzy/settings.py
+++ friendzy/settings.py
@@ -190,9 +190,9 @@ except IOError:
     
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-maybe = dj_database_url.config()
-if maybe != {}:
-   DATABASES['default'] =  dj_database_url.config()
+#maybe = dj_database_url.config()
+#if maybe != {}:
+#   DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
